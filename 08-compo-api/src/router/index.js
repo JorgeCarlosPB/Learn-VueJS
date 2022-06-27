b4e1289901edd799dae099017f2be4cd65f19ee9
JORGE CARLOS PEREZ BARJA <jorge.carlos.perez.barja@gmail.com>
2022-06-27 01:18:40 -0400

    avance del composition api

--- 08-compo-api/src/router/index.js
+++ 08-compo-api/src/router/index.js
@@ -19,7 +19,22 @@ const routes = [
     path: '/counter',
     name: 'counter',
     component: () => import(/* webpackChunkName: "counter" */ '../views/MyCounter.vue')
-  }
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: () => import(/* webpackChunkName: "users" */ '../views/MyUsers.vue')
+  },
+  {
+    path: '/pokemon-search',
+    name: 'pokemon-search', 
+    component: () => import(/* webpackChunkName: "pokemon-search" */ '../views/SearchPokemon.vue')
+  },
+  {
+    path: '/pokemon/:id',
+    name: 'pokemon-id', 
+    component: () => import(/* webpackChunkName: "pokemon-id" */ '../views/MyPokemon.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ 08-compo-api/src/views/SearchPokemon.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Buscar pokemon: </h1>
+  <form @submit.prevent="onSubmit">
+      <input 
+        type="number" 
+        placeholder="Número del pokemón" 
+        v-model="pokemonId"
+        ref="txtSearchId">
+
+  </form>
+</template>
+
+<script>
+import {ref, onActivated} from 'vue'
+import {useRouter} from 'vue-router'
+//import router from '@/src/router'
+
+export default {
+    setup(){
+
+        const router = useRouter()
+
+
+        const pokemonId = ref(1)
+        const txtSearchId = ref()
+
+        onActivated(()=>{
+            //txtSeachId.value.focus()
+            txtSearchId.value.select ()
+        })
+
+
+        return{
+            pokemonId,
+            txtSearchId,
+
+            onSubmit: () => {
+                router.push({name: 'pokemon-id', params: {id: pokemonId.value}})
+            }
+        }
+
+
+    }
+}
+</script>
+
+<style>
+
+</style>
